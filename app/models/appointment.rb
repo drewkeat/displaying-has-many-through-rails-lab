69a4ee9328be7523e8b4e48c271d0f4dadc8720b
Drew Keat <thedrewkeat@gmail.com>
2021-08-28 23:07:42 -0600

    create appointment view

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
     belongs_to :doctor
     belongs_to :patient
+
+    def schedule
+        self.appointment_datetime.strftime ("%B %e, %Y at %R %p")
+    end
 end
